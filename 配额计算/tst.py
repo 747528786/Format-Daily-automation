270f7d5354ad4ea3470a0c83af43fb7877b1c5cc
747528786 <66994895+747528786@users.noreply.github.com>
2022-05-26 09:40:09 +0800

    Delete tst.py

--- 配额计算/tst.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import json
-df = pd.read_excel(r'C:\Users\ASUS\Desktop\4月至今GT渠道已完结工作流.xlsx')
-result = df.changed_data.apply(lambda x:json.loads(x)).apply(pd.Series)
-df_result = pd.merge(df,result,left_index=True,right_index=True)
-df_result.to_excel(r'C:\Users\ASUS\Desktop\4月至今GT渠道已完结工作流5.18.xlsx')
